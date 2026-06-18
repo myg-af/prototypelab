37b397f2ec075b656248bdbbc5df014c3fd2f1f4
myg-af <mygnyh@gmail.com>
2026-06-18 19:38:56 +0200

    v0.1.78 fix web layout: inject hostElement via loader interceptor

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "306126378"
+    serviceWorkerVersion: "3307837979"
   }
 });
