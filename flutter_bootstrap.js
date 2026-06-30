2da39f8e6d7a164eea1dcc618f931b7b362559a1
myg-af <mygnyh@gmail.com>
2026-06-30 20:40:36 +0200

    v0.1.99 - tech tree: auto summon top, offline reward capacity after HP/Damage

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,7 +38,6 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3280644512"
-  },
-  config: { hostElement: document.querySelector('#app') }
+    serviceWorkerVersion: "4278442923"
+  }
 });
