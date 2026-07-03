42c739b74a4ec7761028c667f4b05a72db40968e
myg-af <mygnyh@gmail.com>
2026-07-03 21:13:14 +0200

    v0.1.113 - Cap clan announcement to 6 lines max
    Adds a hard 6-line cap on the clan announcement editor (input formatter
    blocking extra newlines) and matching maxLines/ellipsis on the banner
    display, and lowers the character limit from 300 to 200.

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,7 +38,7 @@ _flutter.buildConfig = {"engineRevision":"035316565ad77281a75305515e4682e6c4c6f7
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3927392897"
+    serviceWorkerVersion: "772510807"
   },
   config: { hostElement: document.querySelector('#app') }
 });
